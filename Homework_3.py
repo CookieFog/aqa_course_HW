8b9651f2fa16641d2af7c92c69d745a64aa0d594
CookieFog <inffista@gmail.com>
2024-03-07 12:28:46 +0200

    Homework_3_lesson_3_final_version_1.1

--- Homework_3.py
+++ Homework_3.py
@@ -1,68 +1,47 @@
 # ******************Task#1 date 04.03.2024******************
-# value = 0
-# while value < 5:
-#     value += 1
-#     Enter_word = str(input("awaiting word"))
-#     new_word = Enter_word[::-1]
-#     if Enter_word == new_word:
-#         print("+")
-#         value += 5
-#     else:
-#         print("-")
-#         print("word is not polyndrome")
-# print("Ended operation!")
+value = 0
+while value < 5:
+    value += 1
+    Enter_word = str(input("awaiting word"))
+    new_word = Enter_word[::-1]
+    if Enter_word == new_word:
+        print("+")
+        value += 5
+    else:
+        print("-")
+        print("word is not polyndrome")
+print("Ended operation!")
 
 
 # ******************Task#2 date 04.03.2024******************
-# value = 0
-# while value < 5:
-#     value += 1
-#     text = str(input("awaiting text"))
-#     word = str(input("awaiting word"))
-#
-#     if word not in text:
-#         print("NO")
-#     else:
-#         print("YES")
-#         value += 5
-# print("Ended operation!")
+value = 0
+while value < 5:
+    value += 1
+    text = str(input("awaiting text"))
+    word = str(input("awaiting word"))
+
+    if word not in text:
+        print("NO")
+    else:
+        print("YES")
+        value += 5
+print("Ended operation!")
 
 # ******************* Task#3 date 04.03.2024*****************
-# value = 0
-# while value < 5:
-#     value += 1
-#     email = str(input("Please enter valid email"))
-#
-#     if "@" and "." not in email:
-#
-#         print("NO,Please enter valid email form")
-#     else:
-#         print("validation successful!")
-#         value += 5
-# print("Ended operation!")
+value = 0
+while value < 5:
+    value += 1
+    email = str(input("Please enter valid email"))
+
+    if "@" and "." not in email:
+
+        print("NO,Please enter valid email form")
+    else:
+        print("validation successful!")
+        value += 5
+print("Ended operation!")
 
 # ******************* Task#4 date 05.03.2024*****************
-
-# value = 0
-# while value < 5:
-#     text = str(input("Please enter text"))
-#     splited = text.split(' ')
-#     value +=1
-#     if len(splited) < 3:
-#         print("Error!. Text shoud to contain more than 3 words !")
-#         print("Your text have less than 3 elements")
-#         print(text.count(' '))
-#         print("current elements")
-#         print(text.split(' '))
-#     else:
-#         print("Successful! last 3 words from list")
-#         print(text.count(' '))
-#         print("current elements")
-#         print(text.split(' '))
-#         value += 5
-# print("Ended operation!")
-
-
 
 value = 0
 while value < 5:
