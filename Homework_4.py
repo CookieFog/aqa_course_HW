2f60a339edb636b21035fef7e4bfdcf881f7f263
CookieFog <inffista@gmail.com>
2024-03-07 16:35:55 +0200

    Homework_4_version_1.1

--- Homework_4.py
+++ Homework_4.py
@@ -1,21 +1,32 @@
 # Homework task #1 date 07.03.2024
 
-list_1 = ["john", "marta", "james", "amanda", "marianna"]
-list_2 = []
-Name_john = list_1.pop(list_1.index("john",))
-Name_marta = list_1.pop(list_1.index("marta"))
-Name_james = list_1.pop(list_1.index("james"))
-Name_amanda = list_1.pop(list_1.index("amanda"))
-Name_marianna = list_1.pop(list_1.index("marianna"))
-
-list_2.append(Name_john.capitalize())
-list_2.append(Name_marta.capitalize())
-list_2.append(Name_james.capitalize())
-list_2.append(Name_amanda.capitalize())
-list_2.append(Name_marianna.capitalize())
-print(list_2)
+# list_1 = ["john", "marta", "james", "amanda", "marianna"]
+# list_2 = []
+# Name_john = list_1.pop(list_1.index("john",))
+# Name_marta = list_1.pop(list_1.index("marta"))
+# Name_james = list_1.pop(list_1.index("james"))
+# Name_amanda = list_1.pop(list_1.index("amanda"))
+# Name_marianna = list_1.pop(list_1.index("marianna"))
+#
+# list_2.append(Name_john.capitalize())
+# list_2.append(Name_marta.capitalize())
+# list_2.append(Name_james.capitalize())
+# list_2.append(Name_amanda.capitalize())
+# list_2.append(Name_marianna.capitalize())
+# print(list_2)
 
 # Homework task #2 date 07.03.2024
+
+list_3 = ["FirstItem", "FriendsList", "MyTuple"]
+
+list_3[1] = "Friends_list"
+list_3[0] = "First_item"
+list_3[2] = "My_Tuple"
+
+
+print(list_3)
+
+# Homework task #3 date 07.03.2024
 
 
 
