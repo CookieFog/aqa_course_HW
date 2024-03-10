ab8676fed3c5daf4a98f679e707b2dc15c23b717
CookieFog <inffista@gmail.com>
2024-03-10 15:26:32 +0200

    Homework_4_version_date 10.03.2024

--- Lesson_5.py
+++ Lesson_5.py
@@ -17,32 +17,63 @@
 # finally:
 #
 #     print("Calculation process has ended")
-
+#
 
 # Homework_5_2 Lesson_5-------------------------------------------------------------------------------------------------
 
-import math
+# import math
+#
+#
+# class SqrtError(Exception):
+#     pass
+#
+#
+# value = 0
+# while value < 2:
+#
+#     try:
+#         s = float(input("Please choose positive number!"))
+#         if s < 0:
+#             value += 1
+#             raise SqrtError("Error!. For sqrt you can use only positive number")
+#
+#         else:
+#             print(math.sqrt(s))
+#             break
+#     except ValueError as error:
+#         print("value error. Please choose correct number!")
+#         value += 1
+#
+#     finally:
+#         print("Calculation process has ended")
 
-
-class SqrtError(Exception):
+# Homework_5_3 Lesson_5-------------------------------------------------------------------------------------------------
+class Exit(Exception):
     pass
 
 
-value = 0
+value = 1
 while value < 2:
-
     try:
-        s = float(input("Please choose positive number!"))
-        if s < 0:
-            value += 1
-            raise SqrtError("Error!. For sqrt you can use only positive number")
+        First_number = float(input("Please enter first number for calculation process"))
+        operator = input("Please choose operator")
+        Second_number = float(input("Please enter second number for calculation process"))
 
-        else:
-            print(math.sqrt(s))
-            break
+        if operator == "+":
+            print(First_number + Second_number)
+        elif operator == "-":
+            print(First_number - Second_number)
+        elif operator == "*":
+            print(First_number * Second_number)
+        elif operator == "/":
+            print(First_number / Second_number)
+    except ZeroDivisionError as error:
+        print("Operation failed. Please enter right number! ZeroDivisionError")
+
     except ValueError as error:
-        print("value error. Please choose correct number!")
-        value += 1
+        print("Operation failed please enter valid value")
 
+    else:
+        print("OK")
     finally:
-        print("Calculation process has ended")
+        print("Calculation process has successful end")
