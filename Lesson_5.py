f48c57af76584a21cce3ed39697512140af59b84
CookieFog <inffista@gmail.com>
2024-03-10 14:40:58 +0200

    Homework_4_version_date 10.03.2024

--- Lesson_5.py
+++ Lesson_5.py
@@ -1,4 +1,25 @@
-# Homework_5 Lesson_5 --------------------------------------------------------------------------------------------------
+# Homework_5_1 Lesson_5 -----------------------------------------------------------------------------------------------
+
+# import math
+#
+#
+# class SqrtError(Exception):
+#     pass
+#
+#
+# try:
+#     s = float(input("Please choose positive number!"))
+#     if s < 0:
+#         raise SqrtError("Error!. For sqrt you can use only positive number")
+#     else:
+#         print(math.sqrt(s))
+#
+# finally:
+#
+#     print("Calculation process has ended")
+
+
+# Homework_5_2 Lesson_5-------------------------------------------------------------------------------------------------
 
 import math
 
@@ -7,13 +28,21 @@ class SqrtError(Exception):
     pass
 
 
-try:
-    s = float(input("Please choose positive number!"))
-    if s < 0:
-        raise SqrtError("Error!. For sqrt you can use only positive number")
-    else:
-        print(math.sqrt(s))
+value = 0
+while value < 2:
 
-finally:
+    try:
+        s = float(input("Please choose positive number!"))
+        if s < 0:
+            value += 1
+            raise SqrtError("Error!. For sqrt you can use only positive number")
 
-    print("Calculation process has ended")
+        else:
+            print(math.sqrt(s))
+            break
+    except ValueError as error:
+        print("value error. Please choose correct number!")
+        value += 1
+
+    finally:
+        print("Calculation process has ended")
