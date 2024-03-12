f19411f1fa96d6d739e180fab82e045bbe55a9e8
CookieFog <inffista@gmail.com>
2024-03-12 16:10:55 +0200

    Homework_5_final

--- Lesson_5.py
+++ Lesson_5.py
@@ -1,60 +1,58 @@
 # Homework_5_1 Lesson_5 ------------------------------------------------------------------------------------------------
 
-# import math
-#
-#
-# class SqrtError(Exception):
-#     pass
-#
-#
-# try:
-#     s = float(input("Please choose positive number!"))
-#     if s < 0:
-#         raise SqrtError("Error!. For sqrt you can use only positive number")
-#     else:
-#         print(math.sqrt(s))
-#
-# finally:
-#
-#     print("Calculation process has ended")
+import math
+
+
+class SqrtError(Exception):
+    pass
+
+
+try:
+    s = float(input("Please choose positive number!"))
+    if s < 0:
+        raise SqrtError("Error!. For sqrt you can use only positive number")
+    else:
+        print(math.sqrt(s))
+
+finally:
+
+    print("Calculation process has ended")
 
 # Homework_5_2 Lesson_5-------------------------------------------------------------------------------------------------
 
-# import math
-#
-#
-# value = 0
-# while value < 2:
-#
-#     try:
-#         s = float(input("Please choose positive number!"))
-#         if s < 0:
-#             value += 1
-#             raise SqrtError("Error!. For sqrt you can use only positive number")
-#
-#         else:
-#             print(math.sqrt(s))
-#             break
-#     except ValueError as error:
-#         print("value error. Please choose correct number!")
-#         value += 1
-#
-#     finally:
-#         print("Calculation process has ended")
+import math
+
+
+value = 0
+while value < 2:
+
+    try:
+        s = float(input("Please choose positive number!"))
+        if s < 0:
+            value += 1
+            raise SqrtError("Error!. For sqrt you can use only positive number")
+
+        else:
+            print(math.sqrt(s))
+            break
+    except ValueError as error:
+        print("value error. Please choose correct number!")
+        value += 1
+
+    finally:
+        print("Calculation process has ended")
 
 # Homework_5_3 Lesson_5-------------------------------------------------------------------------------------------------
-
-import sys
 
 value = 0
 while value < 2:
     try:
-        first_number = float(input("Please enter first number for calculation process"))
-        # if first_number == 'q':
-        #     sys.exit("You chose to quit the program. GoodBye ")
-
+        first_number = input("Please enter first number for calculation process. For exit enter Exit")
+        if first_number == 'Exit':
+            break
         operator = input("Please choose operator")
         second_number = float(input("Please enter second number for calculation process"))
+        first_number = float(first_number)
 
         if operator == "+":
             print(first_number + second_number)
@@ -80,8 +78,3 @@ while value < 2:
         print("OK")
     finally:
         print("Calculator version 3.2")
-    # reboot_calculation = input("For reboot calculation program press F. For end calculation program press Exit")
-    # if reboot_calculation == 'F':
-    #     print("OK")
-    # if reboot_calculation == 'Exit':
-    #     sys.exit("You chose to quit the program. GoodByE!")
