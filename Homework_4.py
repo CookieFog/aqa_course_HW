645f75674b89336a9cb5ea1b2e87a2e4ff8659b4
CookieFog <inffista@gmail.com>
2024-03-12 16:34:55 +0200

    Homework_5_final_1

--- Homework_4.py
+++ Homework_4.py
@@ -26,7 +26,7 @@ for name, creator in dictionary_1.items():
     print(f"My favorite programming language is {name}. It was created by {creator}")
 del dictionary_1['PHP']
 print(dictionary_1)
-# Homework task #4 date 07.03.2024-------------------------------------------------------------------------
+#Homework task #4 date 07.03.2024-------------------------------------------------------------------------
 list_4 = ['Jack', 'Leon', 'Alice', None, 32, 'Bob']
 for item in list_4:
     if isinstance(item, str):
@@ -39,7 +39,7 @@ for value in types:
         print(value)
         continue
     else:
-        print("loop 'For' is not working with float. This program working only with int! ")
+        print("loop 'For' is not working with dict. This program working only with int! ")
         print("!unexpected error loop is closed!")
     break
 # Homework task #6 date 09.03.2024---------------------------------------------------------------------------
@@ -49,7 +49,7 @@ for x in dict_1: x: 'f'
 
 dict_result[x] = dict_result.get(x, 0) + 1
 ''.join([f'{key},{value}' for key, value in dict_result.items()])
-print(dict_result)
+print(''.join([f'{key},{value}' for key, value in dict_result.items()]))
 
 # # Homework task #7 date 10.03.2024----------------------------------------------------------------------------
 value = 0
@@ -62,22 +62,25 @@ while value < 2:
 
         if base_operator == "+":
             print(first_number + second_number)
-            break
+
         elif base_operator == "-":
             print(first_number - second_number)
-            break
+
         elif base_operator == "/":
             print(first_number / second_number)
-            break
+
         elif base_operator == "*":
             print(first_number * second_number)
-            break
+
 
     except ZeroDivisionError as error:
-        value += 1
+
         print("Operation failed. Please enter right number! ZeroDivisionError")
     except ValueError as error:
-        value += 1
+
         print("Operation failed. Please enter right number! ValueError")
+    finally:
+        value += 1
+
     if value == 2:
         print("Zero attempts left. GoodBye")
