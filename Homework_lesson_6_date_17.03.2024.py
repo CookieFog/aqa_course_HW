9e12bd8bd5f0345086d3559c5d91143c12a8207c
CookieFog <inffista@gmail.com>
2024-03-17 11:32:03 +0200

    Homework_6

--- Homework_lesson_6_date_17.03.2024.py
+++ Homework_lesson_6_date_17.03.2024.py
@@ -1,7 +1,22 @@
+import math
+
+
 # Homework_lesson_6 #1 date 17.03.2024
-input("number")
-def sum_range(start, end):
-    if start > end or end > start:
-        print(sum_range(start, end))
+# def sum_range(start, end):
+#     if start > end:
+#         end, start = start, end
+#     return sum(range(start, end + 1))
+#
+#
+# print(sum_range(4, 15))
+
+# Homework_lesson_6 #2 date 17.03.2024
+
+def square(x):
+    y = 2
+    print('периметр квадрата', '=', x * 4)
+    print('площа квадрата', '=', pow(x, y))
+    print(sqrt(2))
 
 
+square(3)
