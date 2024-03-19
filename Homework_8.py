0666c3d322a87cde3d7428081ebedef8395b077e
CookieFog <inffista@gmail.com>
2024-03-19 14:43:01 +0200

    Homework_8

--- Homework_8.py
+++ Homework_8.py
@@ -1,3 +1,9 @@
 import os
 
-print(os.listdir('./'))
+# print(os.listdir('./'))
+
+# print(os.mkdir('temp/temp_1'))
+
+# os.makedirs('temp/temp_1/temp_2/temp_3')
+
+os.rmdir('temp_1/temp_2/temp_3/temp_1txt/temp_2txt')
