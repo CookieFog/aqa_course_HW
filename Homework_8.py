e69f2957dc519cc92d3f278651a84639c1388840
CookieFog <inffista@gmail.com>
2024-03-19 19:27:40 +0200

    Homework_8

--- Homework_8.py
+++ Homework_8.py
@@ -1,9 +1,6 @@
 import os
 
-# print(os.listdir('./'))
-
-# print(os.mkdir('temp/temp_1'))
-
-# os.makedirs('temp/temp_1/temp_2/temp_3')
-
-os.rmdir('temp_1/temp_2/temp_3/temp_1txt/temp_2txt')
+file = open('C:/Users/user/Desktop/test_file.csv', mode='w')
+# print(file.readlines())
+# print(file.writable())
+file.writelines()
