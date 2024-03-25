640511614dfe8075059958599c4f2c37413e1d3a
CookieFog <inffista@gmail.com>
2024-03-25 12:10:51 +0200

    Homework_7

--- Homework_7.py
+++ Homework_7.py
@@ -1,32 +1,33 @@
 # Homework_7_1
 
-my_list = list(map(lambda x, y: x * y, [1, 2, 3], [4, 5, 6]))
-
-print(my_list)
-
-my_list_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-b = filter(lambda x: x % 2 == 0, my_list_2)
-for x in b:
-    print(x)
+# my_list = list(map(lambda x, y: x * y, [1, 2, 3], [4, 5, 6]))
+#
+# print(my_list)
+#
+# my_list_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#
+# b = filter(lambda x: x % 2 == 0, my_list_2)
+# for x in b:
+#     print(x)
 
 
 # Homework_7_2
-
+def DOD(x:int):
+    return lambda y :
 
 # Homework_7_3
 
-def print_hi():
-    print("Hi")
-
-
-def print_bye():
-    print("Bye")
-
-
-def evaluate():
-    user_input = input("Please enter print_hi() or print_bye()")
-    eval(user_input)
-
-
-evaluate()
+# def print_hi():
+#     print("Hi")
+#
+#
+# def print_bye():
+#     print("Bye")
+#
+#
+# def evaluate():
+#     user_input = input("Please enter print_hi() or print_bye()")
+#     eval(user_input)
+#
+#
+# evaluate()
