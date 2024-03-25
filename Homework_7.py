47e8e2f73adf70465cc457601e1ecdc229a00826
CookieFog <inffista@gmail.com>
2024-03-25 14:51:30 +0200

    Homework_7

--- Homework_7.py
+++ Homework_7.py
@@ -1,47 +1,71 @@
 # Homework_7_1
-
-my_list = list(map(lambda x, y: x * y, [1, 2, 3], [4, 5, 6]))
-
-print(my_list)
-
-my_list_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-b = filter(lambda x: x % 2 == 0, my_list_2)
-for x in b:
-    print(x)
+# def func_map(func, iterable_obj: list):
+#     for item in iterable_obj:
+#         yield func(item)
+#
+#
+# def func_filter(func, iterable_obj: list):
+#     for item in iterable_obj:
+#         if func(item):
+#             yield func(item)
 
 
 # Homework_7_2
-try:
-    def DOD(x: int):
-        return lambda y: pow(x, y)
 
+# def DOD(x: int):
+#     return lambda y: pow(x, y)
+#
+#
+# def test(arg):
+#     if callable(arg):
+#         print('start')
+#         print(arg(2))  # pow(x, y)
+#         print('finish')
+#     else:
+#         print("Hm, something went wrong!")
+#
+#     test(DOD(10))
+#
+#     print("Error544")
 
-    def test(arg):
-        if callable(arg):
-            print('start')
-            print(arg(2))  # pow(x, y)
-            print('finish')
-        else:
-            print("Hm, something went wrong!")
-
-    test(DOD(10))
-except:
-    print("Error544")
 
 # Homework_7_3
+# **************************
+# def print_hi():
+#     result = 'Hi'
+#     return result
+#
+#
+# def print_bye():
+#     result = 'Bye'
+#     return result
+#
+#
+# def sum_of_numbers():
+#     result = 5 + 6
+#     return result
+#
+#
+# func_name = input('Please, enter function name: ')
+# try:
+#     print(eval(func_name))
+# except NameError:
+#     print('There is no such function')
 
-def print_hi():
-    print("Hi")
 
-
-def print_bye():
-    print("Bye")
-
-
-def evaluate():
-    user_input = input("Please enter print_hi() or print_bye()")
-    eval(user_input)
-
-
-evaluate()
+# **************************
+# def print_hi():
+#     print("Hi")
+#
+#
+# def print_bye():
+#     print("Bye")
+#
+#
+# def evaluate():
+#     user_input = input("Please enter print_hi() or print_bye()")
+#     eval(user_input)
+#
+#
+# evaluate()
+# **************************
